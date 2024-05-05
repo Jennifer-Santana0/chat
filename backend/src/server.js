ea2ee822e462aba0ebae7435a295e50bf3233424
Jennifer Santana <jennifersouzadesantana@gmail.com>
2024-05-05 19:49:37 -0300

    adicionei a conexão no servidor.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,18 @@
-console.log('ola mundo')
+const { WebSocketServer } = require('ws') 
+require('dotenv').config()
+
+
+const wss = new WebSocketServer({ port: process.env.PORT || 8080 })
+
+wss.on('connection', (ws) => {
+
+    ws.on('error', console.error)
+
+    ws.on('message', (data)=>{
+        //quando enviar a mensagem
+        wss.clients.forEach((client) => client.send(data.toString()))
+    })
+
+    console.log('Client connected')
+
+})
